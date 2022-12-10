c3407ba1c4ff67a41881d009e50949dd75c89e5b
Odin ALEXANDRE <odin.alexandre26@gmail.com>
2022-12-10 13:47:33 +0100

    first commit

---
+++ velostan/js/script.js
@@ -0,0 +1,7 @@
+
+// script pour le menu hamburger
+document.querySelector(".menuhamburger").onclick = function(){
+    document.querySelector(".navbar").classList.toggle("active");
+    document.querySelector(".menu").classList.toggle("active");
+}
+
